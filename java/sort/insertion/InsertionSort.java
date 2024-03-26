fed66f04ba373af5ccb897d5d5e4995483d8cd16
Lee Clark <wymux@shoash.com>
2024-03-26 00:45:44 +0000

    java: Insertion Sort modified
    Signed-off-by: Lee Clark <wymux@shoash.com>

--- java/sort/insertion/InsertionSort.java
+++ java/sort/insertion/InsertionSort.java
@@ -4,28 +4,29 @@ public class InsertionSort {
 			System.out.print(num + " ");
 		System.out.println();
 	}
-
+	
 	public static void insertionSort(int[] arr) {
-		int n = arr.length;
-
-		for (int i = 1; i < n; i++) {
+		int len = arr.length;
+		for (int i = 1; i < len; i++) {
 			int key = arr[i];
 			int j = i - 1;
 			while (j >= 0 && arr[j] > key) {
 				arr[j + 1] = arr[j];
-				j = j - 1;
+				j--;
 			}
-			arr[j + 1] = key; 
+			arr[j + 1] = key;
 		}
 	}
-
+	
 	public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-
-        System.out.println("Original array:");
-        printArray(arr);
-
-        System.out.println("Sorted array:");
-        printArray(arr);		
+		int[] arr = {64, 34, 25, 12, 22, 11, 90};
+		int[] arr1 = {200, 20};
+		System.out.println("Original array: ");
+		printArray(arr);
+		insertionSort(arr);
+		insertionSort(arr1);
+		System.out.println("Sorted array: ");
+		printArray(arr);
+		printArray(arr1);
 	}
 }
