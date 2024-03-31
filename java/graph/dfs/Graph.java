fd04f225843f9053f862332a5f374f827000a9ff
Lee Clark <wymux@shoash.com>
2024-03-31 16:29:57 +0000

    JAva: DFS
    Signed-off-by: Lee Clark <wymux@shoash.com>

--- java/graph/dfs/Graph.java
+++ java/graph/dfs/Graph.java
@@ -17,6 +17,20 @@ public class Graph {
 		adjList.get(dest).add(src);
 	}
 
+	public void dfsUtil(int v, boolean[] visited) {
+		visited[v] = true;
+		System.out.print(v + " ");
+		for (int neighbor : adjList.get(v))
+			if (!visited[neighbor])
+				dfsUtil(neighbor, visited);
+		
+	}
+
+	public void dfs(int src) {
+		boolean[] visited = new boolean[numVertices];
+		dfsUtil(src, visited);
+	}
+
 	public void printGraph() {
 		for (int i = 0; i < numVertices; i++) {
 			System.out.print("Vertex " + i + ": ");
@@ -36,7 +50,12 @@ public class Graph {
 		graph.addEdge(2, 4);
 		graph.addEdge(3, 4);
 
+		System.out.println("Graph:");
 		graph.printGraph();
+
+		System.out.println("DFS traversal starting from vertex 0:");
+		graph.dfs(0);
+
 			
 	}
 }
