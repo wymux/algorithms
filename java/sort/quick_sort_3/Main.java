78a7bee27afcc9acfe9046e71744a2fe50bfdd43
Lee Clark <wymux@shoash.com>
2024-04-04 02:44:47 +0000

    Java: 3-way quicksort

---
+++ java/sort/quick_sort_3/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+	public static void sort(int[] arr) {
+		sort(arr, 0, arr.length - 1);
+	}
+
+	private static void sort(int[] arr, int low, int high) {
+		if (low < high) {
+			int[] pivots = partition(arr, low, high);
+			sort(arr, low, pivots[0] - 1);
+			sort(arr, pivots[1] + 1, high);
+		}
+	}
+
+	private static int[] partition(int[] arr, int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		int j = high;
+		int p = low - 1;
+		int q = high;
+
+		while (true) {
+			while (arr[++i] < pivot);
+			while (arr[--j] > pivot)
+				if (j == low)
+					break;
+			if (i >= j)
+				break;
+
+			swap(arr, i, j);
+			if (arr[i] == pivot) {
+				p++;
+				swap(arr, p, i);
+			}
+
+			if (arr[j] == pivot) {
+				q--;
+				swap(arr, j, q);
+			}
+		}
+		swap(arr, i, high);
+		int[] pivots = {i, j};
+		int k = low - 1;
+
+		while (k < p) {
+			swap(arr, j, k);
+			k++;
+			j--;
+		}
+
+		while (k > q) {
+			swap(arr, i, k);
+			k--;
+			i++;
+		}
+
+		return pivots;
+	}
+
+	private static void swap(int arr[], int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public static void main(String[] args) {}
+}
